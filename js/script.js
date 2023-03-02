b28d52c3cc8601a4437a10e9a4fd840e19298e7c
Michael Yelich <myelich@hawk.iit.edu>
2023-03-02 11:36:49 -0600

    spacing fixed, and starting the additon of a radio button

--- js/script.js
+++ js/script.js
@@ -2,7 +2,5 @@ function init(){
     function Submition() {
 		window.alert("Thank you for your information");
 	}
-	var submitbt = document.getElementById('submit');
-	submitbt.addEventListener('click', Submition);
 }
 window.addEventListener('load', init);
