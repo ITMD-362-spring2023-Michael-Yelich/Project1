387d7b6d899c4a79ddfea0cdec42c41f4d325b2c
Michael Yelich <myelich@hawk.iit.edu>
2023-03-02 15:38:16 -0600

    increasing Terms and conditions

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ function init(){
 	};
   };
   function ListTerms(){
-    window.alert("By completing this survey you agree to forfet all rights to sue over the use of this data by our compnay. Furthermore, by agreeing to this you agree to recive the company newsletter");
+    window.alert("By completing this survey you agree to allow the company to use all data assocated with you from shopping at our store including but not limited to: CreditCard Use, Purchase Habits, and secuirty footage. Furthermore, you agree to forfeit the right to sue over the use of this data by our compnay. Lastly, by agreeing to this you agree to recive the company newsletter");
   }
   var submitbt = document.getElementById('submitbt');
   submitbt.addEventListener('click',Submition);
