49e1b913a6bcea4397e315ae42007da1596c12d6
Michael Yelich <myelich@hawk.iit.edu>
2023-03-02 15:29:08 -0600

    terms and condition made cooler

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ function init(){
 	};
   };
   function ListTerms(){
-    window.alert("By completing this survey you agree to forfet all rights to sue over the use of this data by our compnay. Furthermore, by agreeing to this you agree to recive the company newsletter.");
+    window.alert("By completing this survey you agree to forfet all rights to sue over the use of this data by our compnay. Furthermore, by agreeing to this you agree to recive the company newsletter");
   }
   var submitbt = document.getElementById('submitbt');
   submitbt.addEventListener('click',Submition);
