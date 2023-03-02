f9b0d47e6a2ba3147c8e85315159782bf9d544e5
Michael Yelich <myelich@hawk.iit.edu>
2023-03-02 11:57:52 -0600

    submition now working without form command

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 function init(){
-    function Submition() {
-		window.alert("Thank you for your information");
+  function Submition() {  
+	var check = document.getElementById('T+C');
+    if(check.checked){
+      window.alert("Thank you for your information");
 	}
-}
+    else{
+	  window.alert("Please agree to the terms and conditions");
+	};
+  };
+  var submitbt = document.getElementById('submitbt');
+  submitbt.addEventListener('click',Submition);
+};
 window.addEventListener('load', init);
