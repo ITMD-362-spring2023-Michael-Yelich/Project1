3dab1c7f783aa0dacd780b6971c094230d00d284
Michael Yelich <myelich@hawk.iit.edu>
2023-03-02 12:07:44 -0600

    hide newsletter in terms and conditions

--- js/script.js
+++ js/script.js
@@ -8,7 +8,12 @@ function init(){
 	  window.alert("Please agree to the terms and conditions");
 	};
   };
+  function ListTerms(){
+    window.alert("By completing this survey you agree to forfet all rights to sue over the use of this data by our compnay. Furthermore, by agreeing to this you agree to recive the company newsletter.");
+  }
   var submitbt = document.getElementById('submitbt');
   submitbt.addEventListener('click',Submition);
+  var termsbt = document.getElementById('Terms');
+  termsbt.addEventListener('click',ListTerms);
 };
 window.addEventListener('load', init);
