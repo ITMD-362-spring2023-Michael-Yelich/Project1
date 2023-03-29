3b3d366d60d39fafaf0408c718a88664e95b8225
Michael Yelich <myelich@hawk.iit.edu>
2023-03-29 11:08:28 -0500

    change submit to complete survey

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ function init(){
   function ListTerms(){
     window.alert("By completing this survey you agree to allow the company to use all data assocated with you from shopping at our store including but not limited to: CreditCard Use, Purchase Habits, and secuirty footage. Furthermore, you agree to forfeit the right to sue over the use of this data by our compnay. Lastly, by agreeing to this you agree to recive the company newsletter");
   }
-  var submitbt = document.getElementById('submitbt');
+  var submitbt = document.getElementById('CompleteSurveybt');
   submitbt.addEventListener('click',Submition);
   var termsbt = document.getElementById('Terms');
   termsbt.addEventListener('click',ListTerms);
